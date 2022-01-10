f55354b29e5d9ba7a8aeabfc332df9c412e2509f
Feihong Hsu <feihong.hsu@gmail.com>
2022-01-09 22:14:38 -0600

    Add script to generate a simple page

---
+++ Makefile
@@ -0,0 +1,5 @@
+zip:
+	cd input && zip -r ../tracks.zip *
+
+page:
+	python generate_page.py
---
+++ generate_page.py
@@ -0,0 +1,30 @@
+from pathlib import Path
+import json
+
+from jinja2 import Template
+
+tracks = json.loads(Path('input/tracks.json').read_bytes())
+output_file = Path('index.html')
+
+tmpl = Template("""
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>2021年最值得听的歌曲</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no, viewport-fit=cover">
+  </head>
+  <body>
+    <h1>2021年最值得听的歌曲</h1>
+    <a href="tracks.zip">Download ZIP file</a>
+    <ol>
+    {% for track in tracks %}
+      <li> <a href="{{ track['comments'] }}" target="_blank"> {{ track['title'] }} - {{ track['artist'] }} </a> </li>
+    {% endfor %}
+    </ol>
+  </body>
+</html>
+""")
+
+output_file.write_text(tmpl.render(tracks=tracks))
+
